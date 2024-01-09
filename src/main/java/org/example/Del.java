5df51ac3f37c226b4541f98b2a4451ce98a6288a
Arastode <ep_nikitenko@mail.ru>
2024-01-09 20:53:56 +0300

    Pechenka

--- src/main/java/org/example/Del.java
+++ src/main/java/org/example/Del.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Del implements Delenie{
     public double del(double num1, double num2) {
-        double c = num1 * num2;
+        double c = num1 / num2;
         return c;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,14 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("PrivetikiPistoletiki");
+        double num1=10;
+        double num2=10;
+        Del del = new Del();
+        Substract min = new Substract();
+        Summa plus = new Summa();
+        System.out.println(min.minus(num1, num2));
+        System.out.println(plus.sum(num1,num2));
+        System.out.println(del.del(num1,num2));
     }
 }
+
