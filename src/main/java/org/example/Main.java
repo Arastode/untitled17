e99f708502044bec98d376da171ebb250841de82
414-12 <denisavr2000@gmail.com>
2024-01-09 20:03:03 +0300

    delenie

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,12 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("PrivetikiPistoletiki");
+        double num1=10;
+        double num2=10;
+        Del del = new Del();
+
+
+        System.out.println(del.del(num1,num2));
     }
 }
+
