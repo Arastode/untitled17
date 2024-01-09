019442587ab331096d79ef617d2dec958da7d828
Arastode <ep_nikitenko@mail.ru>
2024-01-09 20:00:28 +0300

    Slozenie

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
-public class Main {
+public class Main  {
     public static void main(String[] args) {
         System.out.println("PrivetikiPistoletiki");
+        double num1 = 10;
+        double num2 = 10;
+        Summa summa = new Summa();
+        System.out.println(summa.sum(num1, num2));
     }
 }
---
+++ src/main/java/org/example/Plus.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public interface Plus  {
+double sum(double num1, double num2);
+
+}
